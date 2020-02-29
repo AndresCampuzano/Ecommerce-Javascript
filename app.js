4c03123e7f7791927c6a1b61e4342252ece851f1
Andres Campuzano <campuxano88@gmail.com>
2020-02-29 06:49:41 -0500

    User can search products between ranges.

--- app.js
+++ app.js
@@ -2,51 +2,75 @@ let productsArray = [
   {
     id: 1,
     name: "SPACEX T-SHIRT",
-    product: "Shirt",
+    product: "shirt",
     price: 22,
     color: "black",
-    image: "./assets/img/spacex_black.webp"
+    image: "https://andrescampuzano.github.io/Ecommerce-Javascript/assets/img/spacex_black.webp"
   },
   {
     id: 2,
     name: "STARMAN T-SHIRT",
-    product: "Shirt",
+    product: "shirt",
     price: 22,
     color: "black",
-    image: "./assets/img/starman.webp"
+    image: "https://andrescampuzano.github.io/Ecommerce-Javascript/assets/img/starman.webp"
   },
   {
     id: 3,
     name: "SPACEX T-SHIRT",
-    product: "Shirt",
+    product: "shirt",
     price: 22,
     color: "blue",
-    image: "./assets/img/spacex_blue.webp"
+    image: "https://andrescampuzano.github.io/Ecommerce-Javascript/assets/img/spacex_blue.webp"
   },
   {
     id: 4,
     name: "SPACEX CAP",
-    product: "Cap",
+    product: "cap",
     price: 10,
     color: "black",
-    image: "./assets/img/cap_black.webp"
+    image: "https://andrescampuzano.github.io/Ecommerce-Javascript/assets/img/cap_black.webp"
   },
   {
     id: 5,
     name: "SPACEX CAP",
-    product: "Cap",
+    product: "cap",
     price: 10,
     color: "blue",
-    image: "./assets/img/cap_blue.webp"
+    image: "https://andrescampuzano.github.io/Ecommerce-Javascript/assets/img/cap_blue.webp"
   },
   {
     id: 6,
     name: "SPACEX CAP",
-    product: "Cap",
+    product: "cap",
     price: 10,
     color: "blue",
-    image: "./assets/img/cap_blue.webp"
-  }
+    image: "https://andrescampuzano.github.io/Ecommerce-Javascript/assets/img/cap_blue.webp"
+  },
+  {
+    id: 7,
+    name: "OCCUPY MARS MUG",
+    product: "mug",
+    price: 12,
+    color: "black",
+    image: "https://cdn.shopify.com/s/files/1/0261/2615/5855/products/Mug_Occupy_Mars_SPXTERRAMUG_FRONT-B_800x.png?v=1567192143"
+  },
+  {
+    id: 8,
+    name: "OCCUPY MARS MUG",
+    product: "mug",
+    price: 12,
+    color: "black",
+    image: "https://cdn.shopify.com/s/files/1/0261/2615/5855/products/Mug_Occupy_Mars_SPXTERRAMUG_FRONT-A_800x.png?v=1567192149"
+  },
+  {
+    id: 9,
+    name: "SPACEX MUG",
+    product: "mug",
+    price: 12,
+    color: "black",
+    image: "https://cdn.shopify.com/s/files/1/0261/2615/5855/products/Mug_Occupy_Mars_SPXTERRAMUG_BACK_800x.png?v=1567192155"
+  },
 ];
 
 function makeList() {
@@ -63,8 +87,9 @@ function makeList() {
     `;
     document.getElementById("allProducts").appendChild(listItem);
   }
-}
-makeList();
+} makeList();
+
+
 // Getting input from user
 function getInputFromUser() {
   var x = document.getElementById("userInputFilterCost").value
@@ -74,11 +99,11 @@ function getInputFromUser() {
 
 //Filtering products by cost
 function filterByCost(value) {
-  var articulosFiltrados = productsArray.filter(function(x) {
+  var productsFilteredArray = productsArray.filter(function(x) {
     return x.price === value; // Makes a new Array.
   });
-  console.log(articulosFiltrados);
-  if (articulosFiltrados.length === 0) {
+  console.log(productsFilteredArray);
+  if (productsFilteredArray.length === 0) {
      return alert(`There are not products with chosen filter, please try again`)
   }
 
@@ -86,15 +111,15 @@ function filterByCost(value) {
     document.getElementById("allProducts").remove("allProducts"); // Removing "allProducts"
     document.getElementById("showProducts").innerHTML = `<div id="filteredProducts"></div>` // Creating "filteredProducts", this line needs to be improved
     
-    for (let i = 0; i < articulosFiltrados.length; i++) {
+    for (let i = 0; i < productsFilteredArray.length; i++) {
       var listItem = document.createElement("div");
       listItem.innerHTML = `
           <div class="product">
-              <h1>${articulosFiltrados[i].name}</h1>
-              <img src=${articulosFiltrados[i].image} alt="">
-              <p>${articulosFiltrados[i].product}</p>
-              <p>Price $${articulosFiltrados[i].price}</p>
-              <p>Color: ${articulosFiltrados[i].color}</p>
+              <h1>${productsFilteredArray[i].name}</h1>
+              <img src=${productsFilteredArray[i].image} alt="">
+              <p>${productsFilteredArray[i].product}</p>
+              <p>Price $${productsFilteredArray[i].price}</p>
+              <p>Color: ${productsFilteredArray[i].color}</p>
           </div>
       `;
       document.getElementById("filteredProducts").appendChild(listItem);
@@ -102,4 +127,52 @@ function filterByCost(value) {
 
   }
   renderFilteredProducts();
+}
+
+
+// Getting input from user
+function getInputFromUserByRange() {
+  var one = document.getElementById("userInputFilterCostOne").value
+  one = parseInt(one)
+	var two = document.getElementById("userInputFilterCostTwo").value
+	two = parseInt(two)
+	//var testOne = 10
+	//var testTwo = 12
+  return filterByCostByRange(one, two)
+};
+
+function filterByCostByRange(one, two) {
+	var productsFilteredArray = productsArray.filter(function(x) {
+    return x.price >= one && x.price <= two; // Makes a new Array.
+  });
+	console.log(productsFilteredArray)
+	if (productsFilteredArray.length === 0) {
+     return alert(`There are not products with chosen filter, please try again`)
+  }
+	
+	function renderFilteredProductsByRange() {
+    document.getElementById("allProducts").remove("allProducts"); // Removing "allProducts"
+    document.getElementById("showProducts").innerHTML = `<div id="filteredProductsByRange"></div>` // Creating "filteredProductsByRange", this line needs to be improved
+    
+    for (let i = 0; i < productsFilteredArray.length; i++) {
+      var listItem = document.createElement("div");
+      listItem.innerHTML = `
+          <div class="product">
+              <h1>${productsFilteredArray[i].name}</h1>
+              <img src=${productsFilteredArray[i].image} alt="">
+              <p>${productsFilteredArray[i].product}</p>
+              <p>Price $${productsFilteredArray[i].price}</p>
+              <p>Color: ${productsFilteredArray[i].color}</p>
+          </div>
+      `;
+      document.getElementById("filteredProductsByRange").appendChild(listItem);
+    }
+
+  }
+  renderFilteredProductsByRange();
+}
+
+function myFunction() {
+  var x = document.getElementById("mySelect").value;
+	console.log(x)
 }
